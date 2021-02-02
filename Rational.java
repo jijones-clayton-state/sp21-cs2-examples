d27554f019df577e697d04808f0412de01db4df0
Jillian Morgan <jillianjones@clayton.edu>
2021-02-01 19:01:00 -0500

    added getters for both attributes

--- Rational.java
+++ Rational.java
@@ -4,4 +4,13 @@ public class Rational{
     private int den;
     //could also be on a single line as private int num, den
     
+    //getters & setters
+    //getters
+    //public attributeType getAttributeName()
+    public int getNum(){
+        return num; //return value of attribute
+    }
+    public int getDen(){
+        return den;
+    }
 }
